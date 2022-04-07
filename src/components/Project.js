5516e3c4c84c5fb5930e23ee85e5f9b47416faf5
ford burgess <fburg6824@gmail.com>
2022-04-07 17:58:30 +0100

    project area halfway

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Project.css'
 
-const Project = ({title, description, height, width, img}) => {
+
+const Project = ({title, description, width, height, img}) => {
   return (
-    <div className='project' style={{height: height, width: width}}>
-      <img src={img} />
+    <div className='this' style={{width: width, height: height}}>
+      <img src={img} alt='img' className='project-img'/>
     </div>
   )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,20 +5,49 @@ import PROJECTS from './projectData'
 
 const Projects = () => {
   return (
-    <div className='project-container'>
-      <h1>
-        Projects
-      </h1>
-      <div>
-        {PROJECTS.map(p => {
-          return (
-            <Project title={p.title}
-            description={p.description}
-            height={p.height}
-            width={p.width}
-            img={p.img}/>
-          )
-        })}
+    <div className='c4'>
+      <div className='project-header'>
+        <h2>
+          Projects
+        </h2>
+      </div>
+      <div className='projects'>
+        <div className='sub-container left'>
+          {PROJECTS.map((p) => {
+            if (p.id === 0)
+              return (
+                <Project title={p.title}
+                description={p.description}
+                height={p.height}
+                width={p.width}
+                img={p.img}/>
+              )
+          })}
+        </div>
+        <div className='sub-container middle' style={{justifyContent: 'space-between'}} id='pieceOfShitDiv'>
+          {PROJECTS.map((p) => {
+            if (p.id === 1 || p.id === 2)
+              return (
+                <Project title={p.title}
+                description={p.description}
+                height={p.height}
+                width={p.width}
+                img={p.img}/>
+              )
+          })}
+        </div>
+        <div className='sub-container right'>
+          {PROJECTS.map((p) => {
+            if (p.id === 3)
+            return (
+              <Project title={p.title}
+              description={p.description}
+              height={p.height}
+              width={p.width}
+              img={p.img}/>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
